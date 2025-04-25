02bb8fa021c8a363b7f58ed3b1e2be5ea331cd03
Mohamed Tah <greyhathacker092@gmail.com>
2025-04-25 17:05:15 +0400

    changed from bg-image to a regular image tag

--- src/app/page.js
+++ src/app/page.js
@@ -92,7 +92,12 @@ export default function Page() {
  
       <div className="h-[75vh] w-full flex flex-col">
 
-        <div className="h-full bg-[url('~/public/images/wave-1.png')] w-full bg-no-repeat animate-jump-in animate-once flex flex-col justify-center min-h-[50vh]">
+        <div className="h-full w-full animate-jump-in animate-once flex flex-col justify-center min-h-[50vh]">
+
+          <img 
+            className="object-cover absolute -z-10 w-full h-full"
+            src="images/wave-1.png"
+          />
 
           <div className="h-1/2 min-h-[40vh] max-w-[800px] shadow-2xl border rounded-3xl p-5 w-full bg-background self-center flex flex-col justify-between">
             <p className="text-4xl">
